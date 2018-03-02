bd72b8b9198c822d34074469c00e467b5379ea15
postables <postables@none.com>
2018-03-01 20:22:45 -0800

    Added factory

---
+++ Contracts/Factory.sol
@@ -0,0 +1,17 @@
+contract Factory is Administration {
+
+	// replace this with the compiled bytecode fro the contract you wish to produce
+	// we mark as constant and private to drasticalyl reduce deployment gas costs
+	bytes constant private code = hex"..";
+
+	function deployToken() external {
+		bytes memory _code = tokenCodeConst;
+		address a;
+		assembly {
+			a := create(0, add(_code, 0x20), mload(_code))
+		}
+		NewContract(a);
+		tokens.push(a);
+	}
+
+}
